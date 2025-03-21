c7fe147b452c7af6ad88dffda75135ddb1a7a4fc
Charles Lechasseur <shiftingbeard@outlook.com>
2025-03-21 16:19:58 -0400

    chore: update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-fn main() {
-    aws_lc_rs::init();
+#[tokio::main]
+async fn main() {
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_dynamodb::Client::new(&config);
+    
+    println!("Region: {:?}", client.config().region());
     
     println!("Hello, world!");
 }
