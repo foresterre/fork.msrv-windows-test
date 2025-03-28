8d2b31e60dd2e31a979acc7858f4aa79af7765fc
Martijn <garm@ilumeo.com>
2025-03-28 08:55:27 +0100

    Improve similarity between cargo-msrv rustup run and the wrapper

--- rustup-run-wrapper/src/main.rs
+++ rustup-run-wrapper/src/main.rs
@@ -2,7 +2,7 @@ use std::process::{Command, Stdio};
 
 fn main() {
     let _ = Command::new("rustup")
-        .args(&["run", "stable", "cargo", "check"])
+        .args(&["run", "stable", "cargo", "check", "--target", "x86_64-pc-windows-msvc"])
         .stdout(Stdio::piped())
         .spawn()
         .unwrap();
